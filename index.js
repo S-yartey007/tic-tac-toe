43aab8c1db3c7a78d6bec695e7a62d76bf10c32b
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-09-19 20:43:55 +0000

    finishing ui of game

--- index.js
+++ index.js
@@ -1,7 +1,88 @@
 const GameBoard = (function() {
  const GameBoard = {
     gameboad: [],
+    playerScore: 0,
+    computerScore: 0,
     Winner: "",
+    cacheDom: function() {
+        this.main = document.querySelector("main");
+        this.form = this.main.querySelector("form");
+        this.select = this.form.querySelector("select")
+        this.gameDom = this.main.querySelector(".gameboard");
+        this.cells = this.gameDom.querySelectorAll("div[data-cell]");
+        this.dialog = this.main.querySelector(".dialog");
+        this.choosePlayer = this.main.querySelector(".choose");
+        this.cancel = this.main.querySelector(".cancelBtn");
+        this.confirm = this.main.querySelector(".confirmBtn");
+        this.newGame = this.main.querySelector(".new-Game");
+        this.newRound = this.main.querySelector(".new-Round");
+        this.declareWinner = this.main.querySelector(".declare-winner");
+        this.declareLoser = this.main.querySelector(".declare-loser");
+        
+    },
+
+    bindevents: function() {
+        this.cells.forEach(cell => {
+            cell.addEventListener("click",this.handleCell.bind(this));
+        })
+        this.choosePlayer.addEventListener("click",this.player.bind(this));
+        this.confirm.addEventListener("click",this.handleConfirm.bind(this));
+        this.cancel.addEventListener("click",this.handleCancel.bind(this));
+        this.dialog.addEventListener("close",this.handleDialogClose.bind(this));
+        this.newRound.addEventListener("click",this.handleNewRound.bind(this))
+        this.newGame.addEventListener("click",this.handNewGame.bind(this))
+
+    },
+
+    handleCell: function(e) {
+        let playerChoice = e.target.getAttribute("data-cell").split("");
+        playerChoice = {row: playerChoice[0],col:playerChoice[1]};
+        this.evaluate(playerChoice,this.computerChoice());
+    },
+
+    player: function(e) {
+        this.dialog.showModal();
+    },
+
+    handleDialogClose: function(e) {
+        e.preventDefault();
+    },
+
+    handleCancel: function(e) {
+       this.dialog.close();
+    },
+
+    handleConfirm: function(e) {
+        this.playerSign = this.select.value;
+        this.createPlayer();
+        console.log(this.playerSign);
+        this.dialog.close();
+    },
+    handleNewRound: function() {
+        this.gameboad = [[" "," "," "],[" "," "," "],[" "," "," "]]
+        this.render();
+       
+    },
+    handNewGame: function() {
+        this.declareWinner.setAttribute("style","display:none");
+        this.declareLoser.setAttribute("style","display:none");
+        GameBoard.init();
+    },
+
+    render: function() {
+       let board = [];
+       this.gameboad.forEach(row => {
+        row.forEach(cell => {
+            board.push(cell);
+        })
+       })
+       console.log(board);
+       for(let i = 0; i < this.cells.length;i++) {
+        this.cells[i].textContent = board[i];
+       }
+       
+    },
+
     gameStatus: function() {
         let counter = 0;
         let regExp = /^[OX]$/
@@ -11,18 +92,16 @@ const GameBoard = (function() {
             })
         })
         return counter;
-    }
-     ,
-
-    createPlayer: function() {
-      this.playerSign = prompt("Enter your symbol:").toUpperCase();
-      this.computerSign = this.playerSign === "X" ? "O" : "X";
     },
 
+    createPlayer: function() {
+      this.computerSign = this.playerSign === "X" ? "O" : "X";
+    },
+   
     round: function() {
       
       const regExp = /^$/;
-      while(this.gameboad[0].some(elem => regExp.test(elem)) || this.gameboad[1].some(elem => regExp.test(elem) || this.gameboad[2].some(elem => regExp.test(elem)))) {
+      /* while(this.gameboad[0].some(elem => regExp.test(elem)) || this.gameboad[1].some(elem => regExp.test(elem) || this.gameboad[2].some(elem => regExp.test(elem)))) {
         if(this.Winner === "player" || this.Winner === "computer") {
             break;
         }
@@ -32,6 +111,8 @@ const GameBoard = (function() {
             col = Number(col);
             this.evaluate({row,col},this.computerChoice());
       }
+      */
+
 
 
     },
@@ -89,65 +170,80 @@ const GameBoard = (function() {
         }
      })
      let r_diagonal = []
-     let l_diagonal = []
      for(let i = 0; i < this.gameboad.length;i++) {
        r_diagonal[i] = this.gameboad[i][i] 
      }
-   /*   for(let i = this.gameboad -1; i > 0; i--) {
-        l_diagonal[i] = this.gameboad[i][i];
-     } */
+     if (this.gameboad[0][2] === this.gameboad[1][1] && this.gameboad[1][1] === this.gameboad[2][0] && this.gameboad[0][2] !== '') {
+         if(this.gameboad[2][0] === this.playerSign) playStatus.diagonal = true;
+         else if(this.gameboad[2][0] === this.computerSign) computerSign.diagonal = true;
+
+      }
+
      if(r_diagonal.every(elem => elem === this.playerSign)) playStatus.diagonal = true;
      else if(r_diagonal.every(elem => elem === this.computerSign)) computerStatus.diagonal = true;
 
      if(playStatus.row || playStatus.col || playStatus.diagonal) {
-        this.Winner = "player"
+        this.playerScore++;
         console.log("You won");
+        console.log(this.playerScore);
      } else if(computerStatus.row || computerStatus.col || computerStatus.diagonal) {
-        this.Winner = "computer"
-        console.log("You lose")
+        this.computerScore++;
+        console.log("You lose");
+        console.log(this.computerScore);
      } 
-     
+     if(this.playerScore >= 5 || this.computerScore >= 5) {
+        this.Winner = this.playerScore > this.computerScore ? "Player" : "Computer";
+        if(this.Winner === "Player") {
+            this.declareWinner.setAttribute("style","display:block");
+            console.log("WINNER")
+        } else {
+            this.declareLoser.setAttribute("style","display:block");
+        }
+     }
 
     },
 
 
     evaluate: function(playerChoice,computerChoice) {
-        let p_row = playerChoice.row;
-        let p_col = playerChoice.col;
-        let c_row = computerChoice.row;
-        let c_col = computerChoice.col;
+        let p_row = Number(playerChoice.row);
+        let p_col = Number(playerChoice.col);
+        let c_row = Number(computerChoice.row);
+        let c_col = Number(computerChoice.col);
         const regExp = /^[XO]$/
         
-        console.log([p_row,p_col],[c_col,c_row])
+        console.log(p_row,p_col,c_row,c_col);
         
-        if(p_row === c_row && c_col === p_col){
-            this.print();
+         if(p_row === c_row && c_col === p_col){
+            this.render();
             
         } else if(p_row > 2 || c_row > 2 ||  c_col > 2 || p_col > 2) {
-            this.print();
+            this.render();
 
-        } else if(regExp.test(this.gameboad[p_row][p_col]) || regExp.test(this.gameboad[p_row][p_col])  ) {
-            this.gameboad[p_row][p_col] = this.playerSign;
-            this.gameboad[c_row][c_col] = this.computerSign;
-            this.print();
+        } else if(regExp.test(this.gameboad[c_row][c_col]))  {
+            this.render();
+           return;
 
         } else {
-            this.gameboad[c_row][c_col] = this.computerSign;
             this.gameboad[p_row][p_col] = this.playerSign;
-            this.print();
+            this.gameboad[c_row][c_col] = this.computerSign;
+            this.render();
         }
         if(this.gameStatus() >= 9) {
             console.log("Draw");
             return;
          }
-         this.checkWinner();
+         this.checkWinner(); 
     },
 
     init: function() {
-        this.gameboad = [["","",""],["","",""],["","",""]]
-        this.createPlayer();
-        this.print();
-        this.round();
+        this.cacheDom();
+        this.bindevents();
+        this.gameboad = [[" "," "," "],[" "," "," "],[" "," "," "]];
+        this.render();
+        this.playerScore = 0,
+        this.computerScore = 0,
+        this.Winner = "";
+        
 
     }
 
@@ -155,5 +251,3 @@ const GameBoard = (function() {
  GameBoard.init();
  return GameBoard;
 })()
-
-console.log(GameBoard)
